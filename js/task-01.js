668e0f102a5e9e1ba3b1ac4b978698df723c89e5
Aleksandr Pavlov <pavlovaleksandr@ukr.net>
2022-05-10 15:10:12 +0300

    10.05

--- js/task-01.js
+++ js/task-01.js
@@ -1,20 +1,23 @@
 const category = categories.children;
 console.log(`Number of categories:`, category.length);
 
+
+
+
+
 const categoryTitle = categories.querySelectorAll(`h2`);
 console.log(categoryTitle);
 const elements = categories.querySelectorAll(`ul`);
 
+    
+    
 
-const getNames = categoryTitle[0].textContent;
-    
-    
-console.log("Масив категорій ", getNames);
 
 
 const getCategoriesNames = (names) => {
+    
     let message;
-    if (getNames.includes(names)) {
+    if (categoryTitle.includes(names)) {
         return message = `Category: ${names}`;
     } return message = `${names} - такої категорії не існує`
 };
@@ -29,10 +32,10 @@ console.log(getCategoriesNames('Technologies'));
 console.log(getCategoriesNames('wqreqr'));
 
 
-// 1. Приймає елемент категорії;
-// 2. Перевіряє чи є така категорія.
-// 3. Рахує кількість елементів в цій категорії
-// 4. Повертає імя категорії та кількість елементів.
+// // 1. Приймає елемент категорії;
+// // 2. Перевіряє чи є така категорія.
+// // 3. Рахує кількість елементів в цій категорії
+// // 4. Повертає імя категорії та кількість елементів.
 
 
 
--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,21 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+console.log(ingredients)
+const itemContainerEl = document.querySelector(`.ingredients`);
+
+const makeIngredientsCard = (ingredients)=> {
+  const ingredientsEl = document.createElement(`li`);
+  ingredientsEl.textContent = ingredients;
+  ingredientsEl.classList.add(`item`);
+
+  return ingredientsEl;
+  
+};
+console.log(makeIngredientsCard(ingredients[1]))
+
+
+const items = ingredients.map(makeIngredientsCard);
+
+console.log(items);
+itemContainerEl.prepend(items);
